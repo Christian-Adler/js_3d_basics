24484e56358f3c9c59ada6a03c9ad19280b24e5b
Christian Adler <chris.adler@gmx.de>
2024-07-26 06:48:34 +0200

    use light sensitive material

--- main.js
+++ main.js
@@ -16,16 +16,24 @@ camera.position.z = 2;
 const scene = new THREE.Scene();
 
 const geo = new THREE.IcosahedronGeometry(1, 2);
-const mat = new THREE.MeshBasicMaterial({
-  color: 0xccff
+// BasicMaterial don't interact with lights
+// const mat = new THREE.MeshBasicMaterial({
+//   color: 0xccff
+// });
+// StandardMaterial interacts with lights
+const mat = new THREE.MeshStandardMaterial({
+  color: 0xffffff,
 });
 const mesh = new THREE.Mesh(geo, mat);
 scene.add(mesh);
 
+const hemiLight = new THREE.HemisphereLight(0xffffff, 0x000000);
+scene.add(hemiLight);
+
 function animate(t = 0) {
   requestAnimationFrame(animate);
 
-  mesh.scale.setScalar(Math.cos(t * 0.001) + 1);
+  // mesh.scale.setScalar(Math.cos(t * 0.001) + 1);
 
   renderer.render(scene, camera);
 }
