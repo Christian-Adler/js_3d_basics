7bda5cc2bf5e30ee73446a2d771a536cfa266f27
Christian Adler <chris.adler@gmx.de>
2024-07-26 13:03:39 +0200

    initial wormhole prepared scene

---
+++ main_wormhole.mjs
@@ -0,0 +1,54 @@
+import * as THREE from "three";
+import {OrbitControls} from 'jsm/controls/OrbitControls.js';
+
+let width = window.innerWidth;
+let height = window.innerHeight;
+
+const renderer = new THREE.WebGLRenderer({antialias: true});
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
+renderer.outputColorSpace = THREE.LinearSRGBColorSpace; // important
+document.body.appendChild(renderer.domElement);
+
+const camera = new THREE.PerspectiveCamera(75, 1.5, 0.1, 1000);
+camera.position.z = 5;
+
+const updateWorldSettings = () => {
+  width = window.innerWidth;
+  height = window.innerHeight;
+  camera.aspect = width / height;
+  camera.updateProjectionMatrix();
+  renderer.setSize(width, height);
+};
+
+window.addEventListener('resize', updateWorldSettings, false);
+updateWorldSettings();
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.01;
+
+const scene = new THREE.Scene();
+// const loader = new THREE.TextureLoader();
+
+const geo = new THREE.BoxGeometry();
+const mat = new THREE.MeshStandardMaterial({
+  color: 0xffff00,
+  // flatShading: true,
+});
+const mesh = new THREE.Mesh(geo, mat);
+scene.add(mesh);
+
+const hemiLight = new THREE.HemisphereLight(0xffffff, 0x505050);
+scene.add(hemiLight);
+
+
+function animate(/*t = 0*/) {
+  // Scene Updates
+
+  // Render & loop
+  renderer.render(scene, camera);
+  controls.update();
+  requestAnimationFrame(animate);
+}
+
+animate();
